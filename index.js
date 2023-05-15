15fade083b594804909a3b6862d48da263818cb5
Abhishek <abhishekchouhannk@gmail.com>
2023-05-14 22:33:33 -0700

    test to make sure axios is running properly

---
+++ index.js
@@ -0,0 +1,7 @@
+const setup = async () => {
+    // test out poke api using axios here
+    const res = await axios.get('https://pokeapi.co/api/v2/pokemon/1')
+    console.log(res.data);
+}
+  
+$(document).ready(setup);

50198e3a6ea88889b92613a72bf2bc8687cd8dad
Abhishek <abhishekchouhannk@gmail.com>
2023-05-15 22:58:14 -0700

    made the final functionality work: switching back from checkboxes to main page with all pokemons is no more buggy can scroll through displayed list of filtered pokemons

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ const PAGE_SIZE = 10;
 let currentPage = 1;
 let pokemons = [];
 let types = [];
+let currentNumberPages;
 
 (async () => {
   try {
     const response = await axios.get("https://pokeapi.co/api/v2/type/");
     let typeData = response.data.results;
-    // console.log(typeData);
     // Use types here
     for (i = 0; i < typeData.length; i++) {
       types[i] = typeData[i].name;
     }
-    // console.log(types);
   } catch (error) {
     console.error("Error fetching types:", error);
     // Handle error here
@@ -83,15 +82,13 @@ const updatePaginationDiv = (currentPage, numPages) => {
     `);
 };
 
-const paginate = async (currentPage, PAGE_SIZE, pokemons) => {
-  console.log(pokemons);
-  let selected_pokemons = pokemons.slice(
+const paginate = async (currentPage, PAGE_SIZE, pokemonsPassed) => {
+  let selected_pokemons = pokemonsPassed.slice(
     (currentPage - 1) * PAGE_SIZE,
     currentPage * PAGE_SIZE
   );
   console.log(selected_pokemons);
   const url = selected_pokemons[0].url;
-  console.log(url);
   const regex = /\/(\d+)\/$/;
   const match = url.match(regex);
 
@@ -99,7 +96,6 @@ const paginate = async (currentPage, PAGE_SIZE, pokemons) => {
   let number;
   if (match) {
     number = match[1] - "0";
-    // console.log(number); // Output: 14
   } else {
     console.log("No number found in the URL");
   }
@@ -111,7 +107,7 @@ const paginate = async (currentPage, PAGE_SIZE, pokemons) => {
   // update status div that tells how many pokemons were fetched and are being displayed at present
   $("#displayed").empty();
   $("#displayed").append(
-    `Displaying Pokemon ${startPokemon}-${endPokemon} of ${pokemons.length}`
+    `Displaying Pokemon ${startPokemon}-${endPokemon} of ${pokemonsPassed.length}`
   );
 
   $("#pokeCards").empty();
@@ -226,10 +222,18 @@ const setup = async (checked) => {
   $("body").on("click", ".numberedButtons", async function (e) {
     currentPage = Number(e.target.value);
 
-    paginate(currentPage, PAGE_SIZE, pokemons);
+    if (checkIfAnyCheckboxChecked()) {
+      paginate(currentPage, PAGE_SIZE, currentPokemons[0]);
 
-    //update pagination buttons
-    updatePaginationDiv(currentPage, numPages);
+      //update pagination buttons
+      updatePaginationDiv(currentPage, currentNumberPages);
+    } else {
+      paginate(currentPage, PAGE_SIZE, pokemons);
+
+      //update pagination buttons
+      updatePaginationDiv(currentPage, numPages);
+    }
+
   });
 };
 
@@ -243,15 +247,11 @@ async function fetchByType(type) {
     `https://pokeapi.co/api/v2/type/${type}`
   );
   typePokemons = response.data.pokemon;
-  // console.log(pokemons);
   let selected_pokemons = [];
   for (i = 0; i < typePokemons.length; i++) {
     selected_pokemons[i] = typePokemons[i].pokemon;
   }
   currentPokemons[counter] = selected_pokemons;
-  // console.log(selected_pokemons)
-  // console.log(names);
-  // console.log(currentPokemons);
   return selected_pokemons;
 }
 
@@ -266,8 +266,8 @@ async function fetchByTypeAndUpdate(checkedValues) {
     const result = findCommonPokemons(currentPokemons);
     if (result.length != 0) {
       paginate(currentPage, PAGE_SIZE, result);
-      const numPages = Math.ceil(result.length / PAGE_SIZE);
-      updatePaginationDiv(currentPage, numPages);
+      currentNumberPages = Math.ceil(result.length / PAGE_SIZE);
+      updatePaginationDiv(currentPage, currentNumberPages);
     } else {
       $('#pokeCards').empty();
       $('#pokeCards').append('No results found');
@@ -294,6 +294,24 @@ function findCommonPokemons(pokemonArrays) {
 
 function isSamePokemon(pokemon1, pokemon2) {
   return pokemon1.name === pokemon2.name && pokemon1.url === pokemon2.url;
+}
+
+function checkIfAnyCheckboxChecked() {
+  let isChecked = false;
+  $('.form-check-input').each(function() {
+    if ($(this).is(':checked')) {
+      isChecked = true;
+      return false; // Exit the loop early if a checkbox is checked
+    }
+  });
+
+  if (isChecked) {
+    console.log('At least one checkbox is checked.');
+    return true;
+  } else {
+    console.log('No checkboxes are checked.');
+    return false;
+  }
 }
 
 
